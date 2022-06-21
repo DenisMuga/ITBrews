9ac8037eed5b8353cc39983340611d3faf838d54
DenisMuga <denismugah5@gmail.com>
2022-06-21 20:42:42 +0300

    Creating .env settings

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ezv_nj6uf1pxj!@7)9g3vlut3=l#=6@^a#c&)ljg0y%hy!h4zr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
