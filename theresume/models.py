5c6a19d25f4fb548a3ea984af223e78161828a77
DenisMuga <denismugah5@gmail.com>
2022-06-21 20:34:25 +0300

    first commit

---
+++ theresume/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Skill(models.Model):
+    class Meta:
+        verbose_name_plural = 'Skills'
+        verbose_name = 'Skill'
+    
+    name = models.CharField(max_length=20, blank=True, null=True)
+    score = models.IntegerField(default=80, blank=True, null=True)
+    image = models.FileField(blank=True, null=True, upload_to="skills")
+    is_key_skill = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
