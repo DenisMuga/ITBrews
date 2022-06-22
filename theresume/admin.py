b6bdd9404ed0914d2d1371f4650c23d1f7d4d90f
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 15:05:26 +0300

    Register model in the admin

--- theresume/admin.py
+++ theresume/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Skill
+from .models import Skill, Testimonial
 
 # Register your models here.
 
-admin.site.register(Skill)
+admin.site.register(Skill),
+admin.site.register(Testimonial),
