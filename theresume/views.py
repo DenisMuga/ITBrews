316959d181ac4de007467f0cbffe8c7c533036f2
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 15:42:59 +0300

    Create a Contact list for api view and cofigure its endpoint

--- theresume/views.py
+++ theresume/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializer import TestimonialSerializer
-from .models import Testimonial
+from .models import ContactProfile, Testimonial
 # from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
@@ -20,6 +20,20 @@ class TestimonialList(APIView):
     
     def post(self, request, format=None):
         serializers = TestimonialSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ContactList(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_contacts = ContactProfile.objects.all()
+        serializers = ContactSerializer(all_contacts, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ContactSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
