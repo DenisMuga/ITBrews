4028c8eb5aa3f72e49bbbf944a4514b14016da6a
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 13:33:57 +0300

    Create and endpoints for the app API view

---
+++ theresume/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('api/testimonial/', views.Testimonial.as_view(),)
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
