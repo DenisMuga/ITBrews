18738ac4cf263ea9cc1e704f980afadf1e66e8de
Nyambura🌻 <bianca@NyambusMacBook.local>
2022-06-23 00:51:03 +0300

    update views

--- theresume/views.py
+++ theresume/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .serializer import PortfolioSerializer, BlogSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Portfolio, Blog
 # Create your views here.
+class PortfolioList(APIView):
+    def get(self, request, format=None):
+        all_portfolios = Portfolio.objects.all()
+        serializers = PortfolioSerializer(all_portfolios, many=True)
+        return Response(serializers.data)  
+    
+class BlogList(APIView):
+    def get(self, request, format=None):
+        all_blogs = Blog.objects.all()
+        serializers = BlogSerializer(all_blogs, many=True)
+        return Response(serializers.data)  
