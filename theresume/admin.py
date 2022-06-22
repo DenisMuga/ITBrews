851030e29b0675e698884a02db0cd9aabe4f89c7
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 15:48:17 +0300

    Register contactprofile  model in the admin

--- theresume/admin.py
+++ theresume/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Skill, Testimonial
+from .models import ContactProfile, Skill, Testimonial
 
 # Register your models here.
 
 admin.site.register(Skill),
 admin.site.register(Testimonial),
+admin.site.register(ContactProfile),
