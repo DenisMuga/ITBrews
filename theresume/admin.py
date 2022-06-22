f9b57ca73cfffae06578a9a7ec0f0b83a20d640c
Fridah-kalee <fridahkalee@gmail.com>
2022-06-22 10:40:27 +0300

    register class certificate in admin.py

--- theresume/admin.py
+++ theresume/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Skill
+from .models import Certificate
 
 # Register your models here.
+admin.site.register(Certificate)
 
-admin.site.register(Skill)
+
