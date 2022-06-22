b2750ddb1b56215871b29c7a296c8db37d3e24b3
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 13:34:42 +0300

    Add API view class for the testimonial list

--- theresume/views.py
+++ theresume/views.py
@@ -1,2 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializer import ProfileSerializer, ProjectSerializer
+# from .permissions import IsAdminOrReadOnly
 
+# Create your views here.
+
+def index(request):
+    return render (request, 'index.html', )
+
+class TestimonialList(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_testimonials = Testimonial.objects.all()
+        serializers = TestimonialSerializer(all_testimonials, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = TestimonialSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
