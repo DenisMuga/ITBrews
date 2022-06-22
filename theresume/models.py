5c224f488f8252d3f27669fc11e98a67d8fa4b67
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 13:05:20 +0300

    Create the testimonial model

--- theresume/models.py
+++ theresume/models.py
@@ -14,3 +14,17 @@ class Skill(models.Model):
     def __str__(self):
         return self.name
 
+class Testimonial(models.Model):
+    class Meta:
+        verbose_name_plural = 'Testimonials'
+        verbose_name = 'Testimonial'
+        ordering = ["name"]
+
+    thumbnail = models.ImageField(blank=True, null=True, upload_to="testimonials")
+    name = models.CharField(max_length=200, blank=True, null=True)
+    role = models.CharField(max_length=200, blank=True, null=True)
+    quote = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def _str_(self):
+        return self.name
