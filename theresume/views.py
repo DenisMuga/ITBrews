5ea8a2968e14c20f33283336a6cd49cc0517566c
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-22 14:30:47 +0300

    Create and update serializer

--- theresume/views.py
+++ theresume/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializer import ProfileSerializer, ProjectSerializer
+from .serializer import TestimonialSerializer
+from .models import Testimonial
 # from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
