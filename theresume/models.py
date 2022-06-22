a02ac442d62b4c979e9f3de83ce6ed1f6bca0d97
Fridah-kalee <fridahkalee@gmail.com>
2022-06-22 13:49:53 +0300

    testing the model

--- theresume/models.py
+++ theresume/models.py
@@ -5,8 +5,7 @@ class Certificate(models.Model):
     course_name = models.CharField(max_length=300)
     institute_name =  models.CharField(max_length=300)
     location = models.CharField(max_length=150)
-    start_date = models.DateField(default='',auto_now_add=True)
-    completion_date = models.DateField(default='',auto_now_add=True)
+    
     
     def save_certificate(self):
         self.save()
