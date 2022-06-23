91913523a340ae6272985c3ec1b60526546b2bac
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-06-23 07:13:52 +0300

    edit views

--- theresume/views.py
+++ theresume/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializer import MediaSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Media
 
 # Create your views here.
+class Media(APIView):
+    def get(self, request, format=None):
+        all_medias = Media.objects.all()
+        serializers = MediaSerializer(all_medias, many=True)
+        return Response(serializers.data)  
