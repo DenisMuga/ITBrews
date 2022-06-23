82d6b40363e89a2d8d6dd509c0a4845c400602e1
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-06-23 12:55:56 +0300

    edit urls

--- theresume/serializer.py
+++ theresume/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Media
 
-class PortfolioSerializer(serializers.ModelSerializer):
+class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
         fields='__all__'
--- theresume/views.py
+++ theresume/views.py
@@ -5,8 +5,25 @@ from rest_framework.views import APIView
 from .models import Media
 
 # Create your views here.
-class Media(APIView):
+class MediaList(APIView):
     def get(self, request, format=None):
         all_medias = Media.objects.all()
         serializers = MediaSerializer(all_medias, many=True)
         return Response(serializers.data)  
+    def post(self, request, format=None):
+        serializers = MediaSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, format=None):
+        media_serializer = MediaSerializer(data=request.data) 
+        if media_serializer.is_valid(): 
+            media_serializer.save() 
+            return Response(media_serializer.data) 
+        return Response(media_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+    def delete(self, request, format=None):
+        all_medias = Media.objects.all().delete()
+        return Response({'message': 'Media was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
