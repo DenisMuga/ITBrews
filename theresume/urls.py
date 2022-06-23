3060cb31f7941eacd0337fdd832d2a8c5125ea42
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-23 13:12:13 +0300

    Add delete and put methods for the testimonials

--- theresume/urls.py
+++ theresume/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('api/testimonial/', views.TestimonialList.as_view()),
     path('api/contactprofile/', views.ContactList.as_view()),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- theresume/views.py
+++ theresume/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -10,17 +11,39 @@ from .models import ContactProfile, Testimonial
 
 class TestimonialList(APIView):
     # permission_classes = (IsAdminOrReadOnly,)
+    # To get all testimonials
     def get(self, request, format=None):
         all_testimonials = Testimonial.objects.all()
         serializers = TestimonialSerializer(all_testimonials, many=True)
         return Response(serializers.data)
     
+    # To get a particular testimonial
+    def get_testimonial(self, pk):
+        try:
+            return Testimonial.objects.get(pk=pk)
+        except Testimonial.DoesNotExist:
+            return Http404
+    
+    # To post testimonials
     def post(self, request, format=None):
         serializers = TestimonialSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # To update testimonials
+    def put(self, request, format=None):
+        serializer = TestimonialSerializer(data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+    # To delete all testimonials
+    def delete(self, request, format=None):
+        all_testimonials = Testimonial.objects.all().delete()
+        return Response({'message': 'Testimonials were deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 class ContactList(APIView):
     # permission_classes = (IsAdminOrReadOnly,)
