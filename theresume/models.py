19b7667a3e9ee5fc318078eac93546b1677a4772
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-06-23 07:13:14 +0300

    create media model

--- theresume/models.py
+++ theresume/models.py
@@ -1,3 +1,6 @@
+from email.mime import image
+from nturl2path import url2pathname
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -13,3 +16,23 @@ class Skill(models.Model):
     
     def __str__(self):
         return self.name
+
+class Media (models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Media Files'
+        verbose_name = 'Media'
+        ordering = ["name"]
+    
+    image = models.ImageField(blank=True, upload_to="media")
+    url = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    is_image = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.url:
+            self.is_image = False
+        super(Media, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
+
